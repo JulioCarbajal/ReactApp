ff0afe9f39d7356979f1a97f4ae6bd00171749a4
Julio Carbjal <julio.carbajal@enkoder.com.mx>
2020-07-16 17:46:06 -0500

    Initial commit

---
+++ components/About/styles.js
@@ -0,0 +1,16 @@
+import {StyleSheet} from 'react-native';
+
+const styles = StyleSheet.create({
+  mainView: {
+    padding: 20,
+    backgroundColor: 'lightblue',
+    borderBottomLeftRadius: 20,
+    borderBottomRightRadius: 20,
+    borderColor: 'blue',
+    borderWidth: 1,
+    borderTopWidth: 0,
+  },
+  myText: {color: 'red'},
+});
+
+export default styles;
